53d5141ad5babf4dd24bd270d0fb5e76a21f0f82
djdt <djdt@djdt.djdt>
2017-03-26 20:43:15 +1100

    added func to reduce size of image

--- include/RGBImage.hpp
+++ include/RGBImage.hpp
@@ -20,6 +20,8 @@ class RGBImage
 		uint32_t _bit_depth;
 		uint32_t _channels;
 
+		RGBPixel GetSquare(uint32_t px, uint32_t py, uint32_t size);
+
 	public:
 		RGBImage();
 		~RGBImage();
@@ -30,10 +32,10 @@ class RGBImage
 		bool LoadPngFile (const std::string& file);
 		bool LoadJpegFile(const std::string& file);
 
+		void ReducePixelCount(uint32_t factor);
+
 		const std::vector<RGBPixel>& pixels() { return _pixels; }
-
 		const std::string& file_path() { return _file_path; }
-
 		uint32_t width() { return _width; }
 		uint32_t height() { return _height; }
 		uint32_t bit_depth() { return _bit_depth; }
